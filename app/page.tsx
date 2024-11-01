3c18bc0312e86e04d932d79936f5da268a2948c4
Andre Wijaya <andrewijaya988@gmail.com>
2024-11-01 12:59:28 +0700

    feat: add hightlight section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import About from "./sections/About";
 import ConnectGroup from "./sections/ConnectGroup";
 import GembalaInfo from "./sections/GembalaInfo";
 import Hero from "./sections/Hero";
+import Highlight from "./sections/Highlight";
 import Services from "./sections/Services";
 import VisiMisi from "./sections/VisiMisi";
 
@@ -14,6 +15,7 @@ export default function Home() {
       <VisiMisi />
       <ConnectGroup />
       <GembalaInfo />
+      <Highlight />
     </main>
   );
 }
