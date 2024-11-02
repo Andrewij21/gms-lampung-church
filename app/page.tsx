0dbf934fe845fab8575482ce3bc5f0eec70e3e06
Andre Wijaya <andrewijaya988@gmail.com>
2024-11-02 08:41:48 +0700

    refactor: move sections folder to components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import About from "./sections/About";
-import ConnectGroup from "./sections/ConnectGroup";
-import GembalaInfo from "./sections/GembalaInfo";
-import Hero from "./sections/Hero";
-import Highlight from "./sections/Highlight";
-import Services from "./sections/Services";
-import VisiMisi from "./sections/VisiMisi";
+import About from "./components/sections/About";
+import ConnectGroup from "./components/sections/ConnectGroup";
+import GembalaInfo from "./components/sections/GembalaInfo";
+import Hero from "./components/sections/Hero";
+import Highlight from "./components/sections/Highlight";
+import Services from "./components/sections/Services";
+import VisiMisi from "./components/sections/VisiMisi";
 
 export default function Home() {
   return (
