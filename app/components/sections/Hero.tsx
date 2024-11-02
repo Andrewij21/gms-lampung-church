d4ec84e2d94b85b760faf174599cc4d6990eaf0a
Andre Wijaya <andrewijaya988@gmail.com>
2024-11-02 09:16:23 +0700

    refactor: make hero section responsive

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -2,7 +2,7 @@ import * as motion from "framer-motion/client";
 import Image from "next/image";
 export default function Hero() {
   return (
-    <section className="relative w-screen h-screen">
+    <section className="relative max-w-screen h-screen">
       <div className="absolute inset-0 overflow-hidden">
         <video
           className="min-h-full object-cover"
@@ -22,17 +22,28 @@ export default function Hero() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{
-          duration: 0,
+          duration: 0.5,
         }}
         className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
       >
-        <Image
-          src={"/logo/welcome-home.svg"}
-          className="h-full w-full"
-          width={500}
-          height={500}
-          alt="welcome"
-        />
+        <div className="hidden sm:block">
+          <Image
+            src={"/logo/welcome-home.svg"}
+            className="h-full w-full"
+            width={500}
+            height={500}
+            alt="welcome"
+          />
+        </div>
+        <div className="block sm:hidden w-screen">
+          <Image
+            src={"/logo/welcome-home-mobile.png"}
+            className="h-full w-full"
+            width={800}
+            height={500}
+            alt="welcome"
+          />
+        </div>
       </motion.div>
     </section>
   );
